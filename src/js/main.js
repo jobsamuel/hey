c7fd425375140234baa57fdcb854b72703c5ef3e
Jobsamuel Núñez <jobsamuel.nunez@outlook.com>
2014-10-17 20:43:35 -0430

    Added new feature
    Scroll to bottom after have displayed the last message

--- src/js/main.js
+++ src/js/main.js
@@ -2,26 +2,32 @@ angular.module('main', [])
 
 .controller('mainCtrl', ['$scope', '$http', function($scope, $http) {
 	
-	$http.get('http://localhost:3000/api/messages')
+	$http.get('/api/messages')
 		.success(function (callback) {
 			$scope.messages = callback;
+		}).error(function (err) {
+			console.log("Error: " + err);
 		});
 
 	var socket = io.connect('http://localhost:3000');
-	
 	$scope.send = function (message) {
 		socket.emit('hey', message);
 		$scope.message = "";
 	}
 
 	socket.on('hey', function (msg) {
-		// Save data locally; avoid the unnecessary server calls.
+		
+		// Save data locally in order to avoid unnecessary server calls.
 		$scope.messages.push({msg: msg});
 
 		// Because the app is making changes outside the Angular context
 		// (i.e. the code that changes models is wrapped outside $apply()),
-		// it's necessary to inform Angular about that change.
+		// it's necessary to inform Angular about it.
 		$scope.$digest();
+
+		// Scroll to bottom after have displayed the last message.
+		var element = document.getElementById("chat");
+		element.scrollTop = element.scrollHeight;
 	});
 
 }]);
